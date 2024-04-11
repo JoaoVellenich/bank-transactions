55ad201a43d01b64ee7627d447bba03c1674e5cb
JoaoVellenich <jpvellenich@gmail.com>
2024-04-11 17:23:48 -0300

    feat: start project

---
+++ banktransactions/src/test/java/com/joaovellenich/banktransactions/BanktransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovellenich.banktransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
