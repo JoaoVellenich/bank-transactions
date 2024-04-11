4d21676131eab7ac3c84f5d43182aa4879110bb2
JoaoVellenich <jpvellenich@gmail.com>
2024-04-11 18:45:23 -0300

    feat: adding function to findByEmail and findByDocument

--- banktransactions/src/main/java/com/joaovellenich/banktransactions/infra/persistence/repository/UserRepository.java
+++ banktransactions/src/main/java/com/joaovellenich/banktransactions/infra/persistence/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.joaovellenich.banktransactions.infra.persistence.repository;
 import com.joaovellenich.banktransactions.infra.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByDocument(String document);
 }
