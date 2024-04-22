da976a2bb6d19188dfeb91fc62ccfeb8f6e5c14a
JoaoVellenich <jpvellenich@gmail.com>
2024-04-22 18:49:57 -0300

    feat: adding functions to user gateway

--- banktransactions/src/main/java/com/joaovellenich/banktransactions/gataweys/adapters/UserGateway.java
+++ banktransactions/src/main/java/com/joaovellenich/banktransactions/gataweys/adapters/UserGateway.java
@@ -2,10 +2,12 @@ package com.joaovellenich.banktransactions.gataweys.adapters;
 
 import com.joaovellenich.banktransactions.domain.User;
 
+import java.math.BigDecimal;
+
 public interface UserGateway {
     User findUserByEmail(String email);
     User findUserByDocument(String document);
     User saveUser(User newUser);
     User findUserById(Long id);
-    User updateUser(User updatedUser);
+    void updateBalance(Long id, BigDecimal newBalance);
 }
