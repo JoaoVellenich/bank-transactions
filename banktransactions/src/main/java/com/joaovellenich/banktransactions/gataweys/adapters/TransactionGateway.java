872bd061103204f09fc05022900184d82b42f79d
JoaoVellenich <jpvellenich@gmail.com>
2024-04-22 18:48:55 -0300

    feat: adding save function to gateway

--- banktransactions/src/main/java/com/joaovellenich/banktransactions/gataweys/adapters/TransactionGateway.java
+++ banktransactions/src/main/java/com/joaovellenich/banktransactions/gataweys/adapters/TransactionGateway.java
@@ -1,4 +1,7 @@
 package com.joaovellenich.banktransactions.gataweys.adapters;
 
+import com.joaovellenich.banktransactions.domain.Transactions;
+
 public interface TransactionGateway {
+    Transactions saveTransaction(Transactions transaction);
 }
