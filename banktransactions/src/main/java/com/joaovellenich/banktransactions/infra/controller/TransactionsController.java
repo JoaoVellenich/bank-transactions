e21d983d05135d80864a2277a327478e1eaf2387
JoaoVellenich <jpvellenich@gmail.com>
2024-04-24 18:35:38 -0300

    feat: calling use case to controller

--- banktransactions/src/main/java/com/joaovellenich/banktransactions/infra/controller/TransactionsController.java
+++ banktransactions/src/main/java/com/joaovellenich/banktransactions/infra/controller/TransactionsController.java
@@ -1,6 +1,9 @@
 package com.joaovellenich.banktransactions.infra.controller;
 
+import com.joaovellenich.banktransactions.domain.Transactions;
+import com.joaovellenich.banktransactions.dto.transaction.createTransaction.CreateTransactionDTOMapper;
 import com.joaovellenich.banktransactions.dto.transaction.createTransaction.CreateTransactionsRequestDTO;
+import com.joaovellenich.banktransactions.gataweys.usecases.CreateTransactionUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/transactions")
 public class TransactionsController {
+    private final CreateTransactionUseCase createTransactionUseCase;
+    private final CreateTransactionDTOMapper createTransactionDTOMapper;
+
+    public TransactionsController(CreateTransactionUseCase createTransactionUseCase, CreateTransactionDTOMapper createTransactionDTOMapper){
+        this.createTransactionUseCase = createTransactionUseCase;
+        this.createTransactionDTOMapper = createTransactionDTOMapper;
+    }
 
     @PostMapping("/")
-    public ResponseEntity createTransaction(@RequestBody CreateTransactionsRequestDTO){
-
+    public ResponseEntity createTransaction(@RequestBody CreateTransactionsRequestDTO createTransactionsRequestDTO){
+        try{
+            Transactions transactions = this.createTransactionUseCase.createTransactionUseCase(createTransactionsRequestDTO);
+            return ResponseEntity.ok().body(this.createTransactionDTOMapper.toResponse(transactions));
+        }catch (Exception err){
+            return ResponseEntity.badRequest().body(err.getMessage());
+        }
     }
 }
