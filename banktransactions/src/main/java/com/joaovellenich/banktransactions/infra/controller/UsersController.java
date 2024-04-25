c828a6e3f5f895f55943a6af1a77f9ca6fc15382
JoaoVellenich <jpvellenich@gmail.com>
2024-04-25 17:44:33 -0300

    feat: adding get route to user controller

--- banktransactions/src/main/java/com/joaovellenich/banktransactions/infra/controller/UsersController.java
+++ banktransactions/src/main/java/com/joaovellenich/banktransactions/infra/controller/UsersController.java
@@ -3,21 +3,35 @@ package com.joaovellenich.banktransactions.infra.controller;
 import com.joaovellenich.banktransactions.domain.User;
 import com.joaovellenich.banktransactions.dto.user.createUser.CreateUserDTOMapper;
 import com.joaovellenich.banktransactions.dto.user.createUser.CreateUserRequestDTO;
+import com.joaovellenich.banktransactions.dto.user.getUser.GetUserDTOMapper;
 import com.joaovellenich.banktransactions.gataweys.usecases.CreateUserUseCase;
+import com.joaovellenich.banktransactions.gataweys.usecases.GetUserUseCase;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UsersController {
-    private CreateUserUseCase createUserUseCase;
-    private CreateUserDTOMapper createUserDTOMapper;
-    public UsersController(CreateUserUseCase createUserUseCase, CreateUserDTOMapper createUserDTOMapper){
+    private final CreateUserUseCase createUserUseCase;
+    private final CreateUserDTOMapper createUserDTOMapper;
+    private final GetUserUseCase getUserUseCase;
+    private final GetUserDTOMapper getUserDTOMapper;
+    public UsersController(CreateUserUseCase createUserUseCase, CreateUserDTOMapper createUserDTOMapper, GetUserUseCase getUserUseCase, GetUserDTOMapper getUserDTOMapper){
         this.createUserUseCase = createUserUseCase;
         this.createUserDTOMapper = createUserDTOMapper;
+        this.getUserUseCase = getUserUseCase;
+        this.getUserDTOMapper = getUserDTOMapper;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getUserById(@PathVariable Long id){
+        try{
+            User user = this.getUserUseCase.GetUserUseCase(id);
+            return ResponseEntity.ok().body(this.getUserDTOMapper.toResponse(user));
+        }catch (Exception error){
+            return ResponseEntity.badRequest().body(error.getMessage());
+        }
     }
 
     @PostMapping("/createUser")
