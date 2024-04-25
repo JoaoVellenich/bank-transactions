24c2e8efa79513f9af993f81d174b975aff284d2
JoaoVellenich <jpvellenich@gmail.com>
2024-04-25 17:44:22 -0300

    feat: inject

--- banktransactions/src/main/java/com/joaovellenich/banktransactions/config/UserConfig.java
+++ banktransactions/src/main/java/com/joaovellenich/banktransactions/config/UserConfig.java
@@ -1,8 +1,10 @@
 package com.joaovellenich.banktransactions.config;
 
 import com.joaovellenich.banktransactions.dto.user.createUser.CreateUserDTOMapper;
+import com.joaovellenich.banktransactions.dto.user.getUser.GetUserDTOMapper;
 import com.joaovellenich.banktransactions.gataweys.adapters.UserGateway;
 import com.joaovellenich.banktransactions.gataweys.usecases.CreateUserUseCase;
+import com.joaovellenich.banktransactions.gataweys.usecases.GetUserUseCase;
 import com.joaovellenich.banktransactions.infra.gateways.UserRepositoryGateway;
 import com.joaovellenich.banktransactions.infra.persistence.mapper.UserMapper;
 import com.joaovellenich.banktransactions.infra.persistence.repository.UserRepository;
@@ -16,6 +18,10 @@ public class UserConfig {
         return new CreateUserUseCase(userGateway);
     }
     @Bean
+    public GetUserUseCase getUserUseCase(UserGateway userGateway){
+        return new GetUserUseCase(userGateway);
+    }
+    @Bean
     public UserGateway userGateway(UserRepository userRepository, UserMapper userMapper){
         return new UserRepositoryGateway(userRepository, userMapper);
     }
@@ -27,4 +33,6 @@ public class UserConfig {
     public CreateUserDTOMapper createUserDTOMapper(){
         return new CreateUserDTOMapper();
     }
+    @Bean
+    public GetUserDTOMapper getUserDTOMapper(){return new GetUserDTOMapper();}
 }
